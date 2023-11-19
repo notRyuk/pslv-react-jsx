b664e1dc4e796054eb99896da398e53710d66b9e
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-11-19 14:25:10 +1100

    Added server start commands so that server is up

--- src/main/server.ts
+++ src/main/server.ts
@@ -3,6 +3,7 @@ import cors from "cors"
 import compression from "compression"
 import apiRouter from "@server/routes"
 import mongoose from "mongoose"
+import { DB_URL, PORT } from "@server/config"
 
 const app = express()
 
@@ -13,4 +14,12 @@ app.use(express.urlencoded({extended: true}))
 
 app.use(apiRouter)
 
-mongoose.connect
+
+mongoose.connect(DB_URL)
+.then(() => {
+    console.log("Connected to the database")
+    app.listen(PORT, () => {
+        console.log("App listening on port:", PORT)
+    })
+})
+.catch(console.log)
