2a17afe6f1cd61b80c6677dddd0cb077b80739bd
notRyuk <amadeusrobot.notryuk@gmail.com>
2023-11-22 03:58:57 +0530

    Updated the client hashing

--- src/utils/hashClient.js
+++ src/utils/hashClient.js
@@ -1,9 +1,21 @@
-import { createHash, timingSafeEqual } from "node:crypto";
-
 export default class Hash {
-    static create = (input) => createHash("sha256").update(input).digest("base64")
+    static async create(input) {
+        const encoder = new TextEncoder();
+        const data = encoder.encode(input);
+        const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+        return btoa(String.fromCharCode(...Array.from(new Uint8Array(hashBuffer))));
+    }
 
-    static compare = (hash, password) => timingSafeEqual(Buffer.from(hash), Buffer.from(this.create(password)))
+    static async compare(hash, password) {
+        const passwordHash = await this.create(password);
+        const hashBuffer = new Uint8Array(atob(hash).split('').map(char => char.charCodeAt(0)));
+        const passwordHashBuffer = new Uint8Array(atob(passwordHash).split('').map(char => char.charCodeAt(0)));
+        return timingSafeEqual(hashBuffer, passwordHashBuffer);
+    }
 
-    static simpleCompare = (val1, val2) => timingSafeEqual(Buffer.from(val1), Buffer.from(val2)) 
+    static simpleCompare(val1, val2) {
+        const buffer1 = new Uint8Array([...val1].map(char => char.charCodeAt(0)));
+        const buffer2 = new Uint8Array([...val2].map(char => char.charCodeAt(0)));
+        return timingSafeEqual(buffer1, buffer2);
+    }
 }
