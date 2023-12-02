939b15e3521b42bf8729ca74691618e129f8df67
PathakTushar <tusharph1@gmail.com>
2023-12-02 14:48:17 +0530

    some changes

--- src/client/components/auth/Logout.jsx
+++ src/client/components/auth/Logout.jsx
@@ -1,21 +1,23 @@
 import { useEffect } from 'react';
 import { selectLoggedInUser, signOutAsync } from './authSlice';
 import { useDispatch, useSelector } from 'react-redux';
-import { Navigate } from 'react-router-dom';
+import { Navigate, useNavigate } from 'react-router-dom';
 
 function Logout() {
   const dispatch = useDispatch();
+  const navigate = useNavigate()
   const user = useSelector(selectLoggedInUser);
 
   useEffect(() => {
     dispatch(signOutAsync());
+    navigate("/signin")
   }, [dispatch]);
 
   // but useEffect runs after render, so we have to delay navigate part
-  return <>
-    {console.log(user)}
-    {user === null && <Navigate to="/signin" replace={true}></Navigate>}
-  </>;
+  return (
+    <>
+    </>
+  );
 }
 
 export default Logout;
