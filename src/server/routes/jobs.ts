81718924e3971304bb543c99d4d3872a2b065232
PathakTushar <tusharph1@gmail.com>
2024-02-18 21:37:50 +0530

    added message model

--- src/server/routes/jobs.ts
+++ src/server/routes/jobs.ts
@@ -12,12 +12,28 @@ app.get("/", verifyToken(), async (_, res) => {
     return res.status(200).send(jobs)
 })
 
-app.get("/user", verifyToken(),  async (_, res) => {
+app.get("/user", verifyToken(), async (_, res) => {
     const { session } = res.locals;
     const job = await Job.find({ from: (session.user as IUser)._id })
-    if(!job)
+    if (!job)
         return res.status(404).json(handler.error(handler.STATUS_404));
     return res.status(200).json(handler.success(job));
 })
+
+// Search jobs by title
+app.get("/search", verifyToken(), async (req, res) => {
+    const { title } = req.query;
+    if (!title) {
+        return res.status(400).json(handler.error(handler.STATUS_404));
+    }
+    const jobs = await Job.find({
+        title: { $regex: new RegExp(title?.toString(), "i") }, // Case-insensitive search
+    }).populate("company");
+    
+    if (!jobs) {
+        return res.status(404).send(handler.error(handler.STATUS_404))
+    }
+    return res.status(200).json(handler.success(jobs));
+});
 
 export default app
