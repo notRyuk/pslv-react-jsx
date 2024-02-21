fe8031b05b2c53a5613a9115e6ac94a8fcd4448d
notRyuk <amadeusrobot.notryuk@gmail.com>
2024-02-22 00:11:03 +0530

    [bug] remove 2nd toastify

--- src/main/client.jsx
+++ src/main/client.jsx
@@ -20,7 +20,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
             <SocketProvider>
               <Router />
               <ToastContainer theme="dark"/>
-              <ToastContainer />
             </SocketProvider>
           </PersistGate>
         </Provider>
